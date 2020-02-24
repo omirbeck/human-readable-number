58e353f16b9fa07ef64f8d68dc53f2cce57f911b
omirbeck <ommirtull@gmail.com>
2020-02-24 22:12:35 +0600

    feat: array find 0-19

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function toReadable (number) {
+  const array = ['zero','one','two','three','four','five','six','seven','eight','nine','ten',
+  'eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen',];
+
+  if(number < 20) return array[number];
   
+  let num = String(number).split('');
+  console.log(array.length - 1)
+  console.log(num.length);
 }
